a01b8daae661cc7cfabc9bd83829f3400df4e01c
andrejs82 <andrejs82@gmail.com>
2017-03-26 12:07:08 -0300

    remote class added to github

---
+++ java/ClassLoaderTests/RemoteClass.java
@@ -0,0 +1,9 @@
+/*
+ *This class will downloaded from network from gig
+ */
+public class RemoteClass implements Runnable {
+
+	public void run(){
+		System.out.println("Hello from remoteClass!");
+	}
+}
