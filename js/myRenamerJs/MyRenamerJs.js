0bde78a2f2e21fca9f49398ecf1d8328e00edadc
andrejs82 <andrejs82@gmail.com>
2018-03-01 05:55:05 -0300

    fixed some problem

--- js/myRenamerJs/MyRenamerJs.js
+++ js/myRenamerJs/MyRenamerJs.js
@@ -5,12 +5,13 @@ const path = './';
 fs.readdir(path, (err, files) => {
   files.forEach(file => {
 	  const newFile = (file.trim().charAt(0).toUpperCase() + file.trim().slice(1))
-	   .replace(/(\.\.)/,'.')
-	   .replace(/(\s+)/,' ')
-       .replace(/(\s)(\d)\D?(\.)/,'$10$2$3')
+	   .replace(/(\.\.)/g,'.')
+	   .replace(/(\s+)/g,' ')
+	   .replace(/№(\d)/g,'№ $1')
+       .replace(/(\s)(\d)([\D]+\.)/,'$10$2$3')
 	   ;
 	   
-	   console.log(path+file, newFile);
+	   console.log(path+file,'-', newFile);
 	   fs.renameSync(path+file, path+newFile);
   });
 })
